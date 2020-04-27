0bc7171321db8e522aec9cc5fde4af78ced7d114
mahdislimane <slimane_mahdi@yahoo.fr>
2020-04-27 16:10:26 +0100

    Updates

---
+++ precache-manifest.650841794a69dc6a6d8fee3828863ff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c8fa6aa407252d0094239c5ae52b06",
+    "url": "/apicheck/index.html"
+  },
+  {
+    "revision": "f06ea02c609785a83e55",
+    "url": "/apicheck/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "26030560cf7a277b4c40",
+    "url": "/apicheck/static/css/main.e4b4b2ed.chunk.css"
+  },
+  {
+    "revision": "f06ea02c609785a83e55",
+    "url": "/apicheck/static/js/2.9cf483ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/apicheck/static/js/2.9cf483ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26030560cf7a277b4c40",
+    "url": "/apicheck/static/js/main.8c9b3324.chunk.js"
+  },
+  {
+    "revision": "0f04d0cc05cf305f5583",
+    "url": "/apicheck/static/js/runtime-main.d507ec6d.js"
+  }
+]);
